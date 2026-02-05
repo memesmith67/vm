76f7c6f3eba16814cf9fc67f2844d6eefafeaf59
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-02-04 21:57:35 -0800

    Update vm.c
    asdfjoaisdoaidjfs

--- vm.c
+++ vm.c
@@ -6,4 +6,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){int x=0,y=16,z=0,i[16],f[512],*t=f;void* p[16];while(y--){i[y]=1;p[y]=&t;}for(;x<512;t[x++]=getchar());while(1){x=t[1];y=t[2];z=*t;switch(z){case 0:i[x]-=i[y];break;case 1:i[x]=i[x]<i[y];break;case 3:p[y]=p[x];break;case 4:i[y]=i[x];break;case 5:p[y]=*(void**)p[x];break;case 6:*(void**)p[y]=p[x];break;case 7:p[y]=(void*)((char*)(p[y]+(int)i[x]));break;case 8:i[x]?p[y]=malloc(i[x]):free(p[y]);i[x]=(p[y]==NULL);break;case 9:fread(p[y],1,i[y],stdin);break;case 10:fwrite(p[y],1,i[y],stdout);fflush(stdout);break;case 11:exit(0);}t+=3;}}
+#define c(x,y) case x: y;break;
+int main(){int x=0,y,z,i[16],f[999],*t=f;void* p[16];*i=1;*p=&t;}while(t[x++]=getchar());while(1){x=t[1];y=t[2];z=*t;switch(z){c(0,exit(0))c(1,i[x]-=i[y])c(2,i[x]=i[x]<i[y])c(3,p[y]=p[x])c(4,i[y]=i[x])c(5,p[y]=*(void**)p[x])c(6,*(void**)p[y]=p[x])c(7,p[y]=(void*)((char*)(p[y]+(int)i[x])))c(8,i[x]?p[y]=malloc(i[x]),free(p[y]);i[x]=(p[y]==NULL))c(9,fread(p[y],1,i[y],stdin))c(10,fwrite(p[y],1,i[y],stdout);fflush(stdout))}t+=3;}}
