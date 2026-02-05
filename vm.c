5493aacdfb0a430ab7858b5f3fa6368623eb3451
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-02-04 22:00:10 -0800

    Update vm.c
    asdofijasoidfjds

--- vm.c
+++ vm.c
@@ -7,4 +7,4 @@
 #include <stdlib.h>
 #include <string.h>
 #define c(x,y) case x: y;break;
-int main(){int x=0,y,z,i[16],f[999],*t=f;void* p[16];*i=1;*p=&t;}while(t[x++]=getchar());while(1){x=t[1];y=t[2];z=*t;switch(z){c(0,exit(0))c(1,i[x]-=i[y])c(2,i[x]=i[x]<i[y])c(3,p[y]=p[x])c(4,i[y]=i[x])c(5,p[y]=*(void**)p[x])c(6,*(void**)p[y]=p[x])c(7,p[y]=(void*)((char*)(p[y]+(int)i[x])))c(8,i[x]?p[y]=malloc(i[x]),free(p[y]);i[x]=(p[y]==NULL))c(9,fread(p[y],1,i[y],stdin))c(10,fwrite(p[y],1,i[y],stdout);fflush(stdout))}t+=3;}}
+int main(){int x=0,y,z,i[16],f[999],*t=f;void* p[16];*i=1;*p=&t;}while(t[x++]=getchar());while(1){x=t[1];y=t[2];z=*t;switch(z){c(0,exit(0))c(1,i[x]-=i[y])c(2,i[x]=i[x]<i[y])c(3,p[y]=p[x])c(4,i[y]=i[x])c(5,p[y]=*(void**)p[x])c(6,*(void**)p[y]=p[x])c(7,p[y]=(void*)((char*)(p[y]+(int)i[x])))c(8,p[y]=malloc(i[x]))c(9,free(p[y]);i[x]=(p[y]==NULL))c(10,fread(p[y],1,i[y],stdin))c(11,fwrite(p[y],1,i[y],stdout);fflush(stdout))}t+=3;}}
