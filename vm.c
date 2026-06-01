f4cc5fe9d6b6b8e98691e27cddf6712be7434711
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-06-01 12:00:04 -0700

    Update vm.c

--- vm.c
+++ vm.c
@@ -1,8 +1,7 @@
+//license:john morris beck 2026 gpl2 @ gnu.org
+//gcc -O3 -march=native -flto vm.c
+//todo: replace malloc and free for mmap an munmap, and make the files be in /dev/shm or fallback to /dev/tmp if shm is unavailable for shared memory between vm processes.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define c(x,y) case x: y;break;
-#define w(x,y) while(x){y;}
-#define a g[x]
-#define b g[y]
-int main(){int x,y,z,t=9999,s=sizeof(int);int *g=malloc(s*t),*h;w('\n'!=(g[--t]=getchar()))t++;w(1,h=g+t;z=h[0];x=h[1];y=h[2];switch(z){c('q',exit(0))c('-',a-=b)c('<',a=a<b)c('r',b=g[a])c('w',g[b]=a)c('a',h=realloc(g,s*a);h==NULL?b=0:(g=h))c('i',z=a;w('\n'!=(*(g+z++)=getchar())))c('o',z=a;w(*(g+z),putchar(*(g+z++))))c('x',t=a-3)}t+=3)}
+int main(){int x,y,z,t=9999,s=sizeof(int);int *g=malloc(s*t),*h;while('\n'!=(g[--t]=getchar()));t++;while(1){h=g+t;z=h[0];x=h[1];y=h[2];switch(z){case 'q':exit(0);break;case '-':g[x]-=g[y];break;case '<':g[x]=g[x]<g[y];break;case 'r':g[y]=g[g[x]];break;case 'w':g[g[y]]=g[x];break;case 'a':h=realloc(g,s*g[x]);h==NULL?g[y]=0:(g=h);break;case 'i':z=g[x];while('\n'!=(*(g+z++)=getchar()));break;case 'o':z=g[x];while(*(g+z)){putchar(*(g+z++));fflush(stdout);}break;case 'x':t g[x]-3;break;}t+=3;}}
