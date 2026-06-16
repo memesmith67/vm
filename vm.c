4ab494233a278995e074f9804bfc556f956b7068
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-06-15 19:12:52 -0700

    Update vm.c

--- vm.c
+++ vm.c
@@ -4,4 +4,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(){int x,y,z,t=9999,s=sizeof(int);int *g=malloc(s*t),*h;while('\n'!=(g[--t]=getchar()));t++;while(1){h=g+t;z=h[0];x=h[1];y=h[2];switch(z){case 'q':exit(0);break;case '-':g[x]-=g[y];break;case '<':g[x]=g[x]<g[y];break;case 'r':g[y]=g[g[x]];break;case 'w':g[g[y]]=g[x];break;case 'a':h=realloc(g,s*g[x]);h==NULL?g[y]=0:(g=h);break;case 'i':z=g[x];while('\n'!=(*(g+z++)=getchar()));break;case 'o':z=g[x];while(*(g+z))putchar(*(g+z++));fflush(stdout);break;case 'x':t g[x]-3;break;}t+=3;}}
+int main(){int x,y,z,t=9999,s=sizeof(int);int *g=malloc(s*t),*h;while('\n'!=(g[--t]=getchar()));t++;while(1){h=g+t;z=h[0];x=h[1];y=h[2];switch(z){case 'q':exit(0);break;case '-':g[x]-=g[y];break;case '<':g[x]=g[x]<g[y];break;case 'r':g[y]=g[g[x]];break;case 'w':g[g[y]]=g[x];break;case 'a':h=realloc(g,s*g[x]);h==NULL?g[y]=0:(g=h);break;case 'i':z=g[x];while('\n'!=(*(g+z++)=getchar()));break;case 'o':z=g[x];while(*(g+z))putchar(*(g+z++));fflush(stdout);break;case 'x':t=g[x]-3;break;}t+=3;}}
